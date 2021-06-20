91c15f2268879162573c89c2dcb8fe3caabf1e14
Braedon Wooding <braedonw@resonatesolutions.com.au>
2021-06-21 01:28:13 +1000

    GoogleLogin

--- ChonkyReviews/Controllers/Accounts.cs
+++ ChonkyReviews/Controllers/Accounts.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ChonkyReviews.Models;
-using ChonkyReviews.Utils;
+using ChonkyReviews.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
